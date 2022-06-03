70d4800a5f405cb9d5d076381dbfefe6cefa7234
0Rikke <henriqueeitelve@gmail.com>
2022-06-03 15:24:36 -0300

    first

---
+++ frontend/cadastro_cli.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=][, initial-scale=1.0">
+    <title>Cadastro cliente</title>
+</head>
+<body>
+    <form action="inserir.php" method="$_POST">
+        <fieldset>
+            <label for="name">Username</label>
+            <input type="text" id="name">
+
+            <label for="email">Email</label>
+            <input type="email" id="email">
+            
+            <label for="senha">Senha</label>
+            <input type="password" id="senha">
+
+            <label for="senhaConf">Confirmar Senha</label>
+            <input type="password" id="senhaConf">
+
+            <button type="submit">Cadastrar</button>
+        </fieldset>
+
+    </form>
+</body>
+</html>

be412daf0c075044d58aff22955a17bc8061f1c3
0Rikke <henriqueeitelve@gmail.com>
2022-06-03 23:34:53 -0300

    quarto

--- frontend/cadastro_cli.php
+++ frontend/cadastro_cli.php
@@ -4,47 +4,62 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=][, initial-scale=1.0">
-    <link rel="stylesheet" href="css/cadastroCli.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Cadastro cliente</title>
 </head>
 <body>
+    <nav>
+        <a href="index.php"><img class = "seta"src="img/setinha.png" alt="seta para voltar a página"></a>
+    </nav>
+    <h1>Cadastro de Usuário</h1>
+    
     <form action="inserir.php" method="$_POST">
-        <fieldset>
-        <div class="grid">  
-            <div class="inp">
-                <label for="name">Nome</label>
-                <input type="text" id="name" placeholder="NameExemple">
+        
+        <div class="inside">
+            <div class="grid">  
+                <div class="inp">
+                    <label for="name">Nome</label>
+                    <input type="text" id="name" placeholder="NameExemple">
+                </div>
+                <div class="inp">
+                    <label for="Lname">Sobrenome</label>
+                    <input type="text" id="Lname" placeholder="lastNameExample">
+                </div>
             </div>
-            <div class="inp">
-                <label for="Lname">Sobrenome</label>
-                <input type="text" id="Lname" placeholder="lastNameExample">
-            </div>
-        </div>
-            <div class="inp">           
-                <label for="email">Email</label>
-                <input type="email" id="email" placeholder="EmailExample@gmail.com">
-            </div>
-        <div class="grid"> 
-            <div class="inp">
-                <label for="senha">Senha</label>
-                <input type="password" id="senha " placeholder="Senha">
-            </div>   
-            <div class="inp"> 
-                <label for="senhaConf">Confirmar Senha</label>
-                <input type="password" id="senhaConf" placeholder="Confirme a Senha">
-            </div>
-        </div>     
+                <div class="inp">           
+                    <label for="email">Email</label>
+                    <input type="email" id="email" placeholder="EmailExample@gmail.com">
+                </div>
+            <div class="grid"> 
+                <div class="inp">
+                    <label for="senha">Senha</label>
+                    <input type="password" id="senha " placeholder="Senha">
+                </div>   
+                <div class="inp"> 
+                    <label for="senhaConf">Confirmar Senha</label>
+                    <input type="password" id="senhaConf" placeholder="Confirme a Senha">
+                </div>
+            </div> 
+            <div class="check">
+                <label for="check">Manter logado</label>
+                <input type="checkbox" name="check" id="check">
+            </div> 
             <div class="inp">
                 <button class="botao" type="submit">Cadastrar</button>
             </div>
-         
+            <div>
+                <p class="inp">Já possui uma conta uma?</p>
+            </div>
+
+            <div class="inp">
+                <button class="botao"><a href="login">Logar</a></button>
+            </div>   
+        </div>
         
-            
-            
-            
-            
-            
-        </fieldset>
     </form>
+
+    <footer class="footer">
+        <p>&copyDesenvolvimentoDeSistemas</p>
+    </footer>
 </body>
 </html>
--- frontend/index.php
+++ frontend/index.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/index.css">
+    <title>Home</title>
 </head>
 <body>
+
+
+
+
+    <div class="footer">
+        <p>&copyDesenvolvimentoDeSistemas</p>
+    </div>
+    
+        
+        
     
 </body>
 </html>

5e51f3af235acfbbb6c331260784fdddc1fedd8c
Henrique Santos <henrique.260204@bento.local>
2022-06-07 16:34:17 -0300

    as

---
+++ frontend/biblioteca.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/biblio.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <nav >
+        <ul id="a">
+            <li><a href=""> Home</a></li>
+            <li style="float:right"><a href="">Logar</a></li>
+            <li style="float:right"><a href="cadastro_cli.php">Cadastrar</a></li>
+        </ul>
+    </nav>
+
+    <div class="biblio">
+
+        <div class="bibli-nav">
+            <div class="orderBy">
+                <ul>
+                    <li>Recentes:<input type="checkbox"></li>
+                    <li>Recentes:<input type="checkbox"></li>
+                    <li>Recentes:<input type="checkbox"></li>
+                </ul>
+            </div>
+            <input type="serch"> <button type="submits">Pesquisar</button>
+        </div>
+
+    </div>
+
+     
+  <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
